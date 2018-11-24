051cf73d98ce557d531d5403d854d2fb282be0e3
Sophia Lau <slau2022@gmail.com>
2018-11-24 14:54:01 -0500

    ha

--- Blegh.py
+++ Blegh.py
@@ -15,34 +15,60 @@ def validContours(edges):
                 return True
     return False
     
+def betterIsolation(leaf):
+    #returns contours of the leaf
+    ph = cv2.imread(leaf)
+    ph = cv2.resize(ph, (300,500))
+    ph = cv2.cvtColor(ph, cv2.COLOR_BGR2GRAY)
+
+    th3 = cv2.bitwise_not(ph) #isolate just the leaf
+
+    ret, th3 = cv2.threshold(th3,15,255,cv2.THRESH_BINARY)#get rid of innards of leaf
+
+    can = cv2.Canny(th3, 300, 600)
+    
+
+   #   th3 = cv2.adaptiveThreshold(th3,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+    #             cv2.THRESH_BINARY,11,2) #get outline of leaf
+    
+    im2, contours, hierarchy = cv2.findContours(can, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.imshow("when you try your best", can)
+    cv2.waitKey(0)
+   
+    print(contours)
+    return contours
+    
 def compareOutlines(leaf1, leaf2):
     
-    ph = cv2.imread(leaf1) #leaf1
-    ph2 = cv2.imread(leaf2) #leaf2
-    ph = cv2.resize(ph, (300, 500))
-    ph2 = cv2.resize(ph2, (300,500))
-    
-    
-    ph = cv2.cvtColor(ph, cv2.COLOR_BGR2GRAY)
-    ph2 = cv2.cvtColor(ph2, cv2.COLOR_BGR2GRAY)
-    
-    edges = cv2.Canny(ph, 300,600)
-    edges2 = cv2.Canny(ph2, 300,600)
+    # ph = cv2.imread(leaf1) #leaf1
+    # ph2 = cv2.imread(leaf2) #leaf2
+    # ph = cv2.resize(ph, (300, 500))
+    # ph2 = cv2.resize(ph2, (300,500))
+    #  
+    # 
+    # ph = cv2.cvtColor(ph, cv2.COLOR_BGR2GRAY)
+    # ph2 = cv2.cvtColor(ph2, cv2.COLOR_BGR2GRAY)
+    # 
+    # edges = cv2.Canny(ph, 300,600)
+    # edges2 = cv2.Canny(ph2, 300,600)
 
+   ##   
+    # modify = 0
+    # 
+    # while not validContours(edges2):
+    #     edges2 = cv2.Canny(ph2, 100,1000-100*modify)
+    #     modify +=1
+    # 
+    # img, cont, hierarchy = cv2.findContours(edges,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    # img2, cont2, hierarchy2 = cv2.findContours(edges2,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    # 
+    # cv2.imshow("base", img)
+    # cv2.waitKey(0)
+    # cv2.imshow("compare", img2)
+    # cv2.waitKey(0)
     
-    modify = 0
-    
-    while not validContours(edges2):
-        edges2 = cv2.Canny(ph2, 100,1000-100*modify)
-        modify +=1
-    
-    img, cont, hierarchy = cv2.findContours(edges,cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    img2, cont2, hierarchy2 = cv2.findContours(edges2,cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    
-    cv2.imshow("base", img)
-    cv2.waitKey(0)
-    cv2.imshow("compare", img2)
-    cv2.waitKey(0)
+    cont = betterIsolation(leaf1)
+    cont2 = betterIsolation(leaf2)
     
     ret = cv2.matchShapes(cont[0],cont2[0],1,0.0)
     print(ret)
@@ -57,7 +83,6 @@ def compareOutlines(leaf1, leaf2):
     return ret
 
 def analyzeLeaf(leaf):
-    img = "LeafShapescopy/Sinuate/oak2.jpg"
     leaftypes = ["Fanned","Sinuate", "Lobed", "Heartshaped", "Ovoid", "Triangular","Rounded","Lanceolate"]
     winValue = None
     winLeaf = None
@@ -68,7 +93,7 @@ def analyzeLeaf(leaf):
             other = "LeafShapescopy/"+feuille+"/"+feuille+str(num)+".jpg"
             print(other)
             closeValue += compareOutlines(leaf, other )
-        closeValue = closeValue/3
+        closeValue = closeValue/3 #averages the values for each type
         if winValue == None:
             winValue = closeValue
             winLeaf = feuille
@@ -78,7 +103,7 @@ def analyzeLeaf(leaf):
     
     return winLeaf
         
-print(analyzeLeaf("leaftrial.jpg"))
+print(analyzeLeaf("leaftrial2.jpg"))
 
 # ph = cv2.imread("leaftrial.jpg")
 # ph = cv2.cvtColor(ph, cv2.COLOR_BGR2GRAY)
@@ -87,6 +112,11 @@ print(analyzeLeaf("leaftrial.jpg"))
 # print(edges)
 # cv2.waitKey(0)
     #see if shape is fanned, sinuate, lobed, heartshaped, ovoid, triangular, rounded or lanceolate
+
+#https://www.pyimagesearch.com/2014/04/07/building-pokedex-python-indexing-sprites-using-shape-descriptors-step-3-6/
+
+
+
 
 
 def countLobes(leaf):
